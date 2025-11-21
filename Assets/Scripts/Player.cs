b2bce7dc60a41874e9618ba488c31e7a90ba89b8
LewinMods <oscar.lewin06@gmail.com>
2025-11-21 09:49:59 +0100

    Växla mellan flera animationer, konstant animation när man står stilla + animation när man går

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,11 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 Direction;
     private int speed = 5;
+    private Animator animator;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     
     void Update()
@@ -15,5 +17,14 @@ public class Player : MonoBehaviour
         Direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         
         rb.linearVelocity = Direction * speed;
+
+        if (Direction != Vector2.zero)
+        {
+            animator.SetBool("Walking", true);
+        }
+        else
+        {
+            animator.SetBool("Walking", false);
+        }
     }
 }
